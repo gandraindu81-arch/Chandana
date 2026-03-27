4bd0a380daeef808a2b61f75e9fcb8e42d1dbe91
gandraindu81-arch <gandraindu81@gmail.com>
2026-03-28 02:04:54 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+<section id="about">
+  <h2>About Chandana</h2>
+  <p>I design elegant and modern dresses. Welcome to my designer portfolio!</p>
+  <img src="images/project1.jpeg" alt="Designer Dress 1" class="about-img">
+</section>
