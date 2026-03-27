fc27ac75da0f354294035becec92ec68e7683b3c
gandraindu81-arch <gandraindu81@gmail.com>
2026-03-28 02:15:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-<section id="about">
-  <h2>About Chandana</h2>
-  <p>I design elegant and modern dresses. Welcome to my designer portfolio!</p>
-  <img src="images/project1.jpeg" alt="Designer Dress 1" class="about-img">
-</section>
+// Smooth scroll for navbar links
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
